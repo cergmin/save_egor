b51809e22474c949792ae216021f7d2bd3245f0f
cergmin <cergmin@gmail.com>
2020-07-13 17:24:37 +1000

    Added writing score to db (dev)

---
+++ create_json.py
@@ -0,0 +1,29 @@
+import json
+
+
+with open('rank.json', 'r') as f:
+    db = json.load(f)
+
+name = 'cergmin'
+ip = '0.0.0.0'
+score = 300
+
+for i in db:
+    if i['name'] == name:
+        if i['ip'] == ip:
+            if i['score'] < score:
+                i['score'] = score
+            with open('rank.json', 'a') as f:
+	            json.dump(db, f)
+            return 'ok'
+        return 'wrong_ip'
+
+db.append({
+    'name': name,
+    'ip': ip,
+    'score': score,
+    'note': ''
+})
+
+with open('rank.json', 'a') as f:
+	json.dump(db, f)
